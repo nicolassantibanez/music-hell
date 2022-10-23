1b73126544cf5efd9df8f5cf35e27406c613e684
Giuliano Celedon <giulianoceledon2206@gmail.com>
2022-10-23 15:35:18 -0300

    se agranda el mapa

--- scripts/nota_roja_1.gd
+++ scripts/nota_roja_1.gd
@@ -19,4 +19,8 @@ func _on_KillerTimer_timeout():
 	queue_free()
 	
 func _on_body_entered(body: Node):
-	queue_free()
+	if body.has_method("take_damage") and body.name == "Protagonista": # and no enemigo
+		body.take_damage()
+	if not body.has_method("take_damage"):
+		queue_free()
+
