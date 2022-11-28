3233e6add6b5015448e089fa381fe1834874b7c4
Santi <nicolassantibanez@ug.uchile.cl>
2022-11-27 22:01:01 -0300

    fix: Fade was blocking everything

--- scripts/fade.gd
+++ scripts/fade.gd
@@ -4,14 +4,19 @@ signal faded
 
 onready var animation_player = $AnimationPlayer
 
-
+func _ready():
+	hide()
 
 func fade_out():
+	show()
 	animation_player.play("fade_out")
 	yield(animation_player, "animation_finished")
 	emit_signal("faded")
+	hide()
 
 func fade_in():
+	show()
 	animation_player.play("fade_in")
 	yield(animation_player, "animation_finished")
 	emit_signal("faded")
+	hide()
