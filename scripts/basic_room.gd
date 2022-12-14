fa820d5ab479881b77ebb4f19e2faa83c131ebc5
Santi <nicolassantibanez@ug.uchile.cl>
2022-12-14 19:19:41 -0300

    feat: Level02 ready!

--- scripts/basic_room.gd
+++ scripts/basic_room.gd
@@ -4,7 +4,8 @@ extends Node2D
 const ENEMY_PATH = preload("res://scenes/enemy_1_level01.tscn")
 
 # Exported variables
-export(int) var enemies_to_spawn = 3
+export(Array, PackedScene) var enemies_list
+export(PoolIntArray) var enemies_to_spawn = []
 
 # Public variables
 var current_enemies = 0
@@ -15,6 +16,7 @@ var _player_entered = false
 # Onready variables
 onready var room_area2d = $RoomArea2D
 onready var enemies_spawn_shape = $EnemiesSpawnArea2D/CollisionShape2D
+onready var enemies_secondary_spawn_shape = $EnemiesSpawnArea2D/CollisionShape2D2
 onready var protagonista = $"%Protagonista"
 onready var gates = [$LeftGate, $RightGate]
 
@@ -29,15 +31,26 @@ func _process(delta):
 		_open_gates()
 
 func _spawn_enemies():
-	for i in range(enemies_to_spawn):
-		var room_x = enemies_spawn_shape.shape.extents.x
-		var room_y = enemies_spawn_shape.shape.extents.y
-		var spawn_pos = Vector2(rand_range(-room_x, room_x) + global_position.x, rand_range(-room_y, room_y) + global_position.y)
-		var enemy = ENEMY_PATH.instance()
-		enemy.position = spawn_pos
-		get_parent().add_child(enemy)
-		enemy.protagonista = self.protagonista
-		current_enemies += 1
+#	var room_x = enemies_spawn_shape.shape.extents.x
+#	var room_y = enemies_spawn_shape.shape.extents.y
+	var spawn_zones = [enemies_spawn_shape]
+	if enemies_secondary_spawn_shape != null:
+		spawn_zones.append(enemies_secondary_spawn_shape)
+	for i in range(len(enemies_list)):
+		var enemy_scene = enemies_list[i]
+		if i >= len(enemies_to_spawn):
+			print("WARNING: Enemy ", enemy_scene, " doesn't have specified how many to spawn!")
+			break
+		for j in range(enemies_to_spawn[i]):
+			var spawn_zone = spawn_zones[randi() % spawn_zones.size()]
+			var room_x = spawn_zone.shape.extents.x
+			var room_y = spawn_zone.shape.extents.y
+			var spawn_pos = spawn_zone.position + Vector2(rand_range(-room_x, room_x) + global_position.x, rand_range(-room_y, room_y) + global_position.y)
+			var enemy = enemy_scene.instance()
+			enemy.position = spawn_pos
+			get_parent().add_child(enemy)
+			enemy.protagonista = self.protagonista
+			current_enemies += 1
 
 func _open_gates():
 	for gate in gates:
